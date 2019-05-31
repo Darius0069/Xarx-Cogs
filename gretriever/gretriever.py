9aa0269b624d1fb6f4a025f86844114738949022
Darius0069 <39631501+Darius0069@users.noreply.github.com>
2019-05-31 08:48:40 -0500

    Update gretriever.py

--- gretriever/gretriever.py
+++ gretriever/gretriever.py
@@ -10,7 +10,7 @@ from redbot.core import commands
 # Libs
 import aiohttp
 
-grapi = "https://dog.ceo/api/breed/retriever/golden/images/random"
+goldenapi = "https://dog.ceo/api/breed/retriever/golden/images/random"
 
 BaseCog = getattr(commands, "Cog", object)
 
@@ -21,17 +21,37 @@ class Gretriever(BaseCog):
     def __init__(self, bot):
         self.bot = bot
         self.session = aiohttp.ClientSession(loop=self.bot.loop)
-        self.grapi = grapi
+        self.goldenapi = goldenapi
 
     @commands.command()
     @commands.cooldown(1, 60, commands.BucketType.guild)
-    async def gretriever(self, ctx):
-        """Shows a Golden Retriever"""
-            async with self.session.get(self.grapi) as r:
+    async def goldendoggo(self, ctx):
+        """Shows a golden retriever"""
+        try:
+            async with self.session.get(self.goldenapi) as r:
                 result = await r.json()
-            await ctx.send(result['file'])
+            await ctx.send(result['message'])
+        except:
+            await ctx.send("API Error")
 
+    @commands.command()
+    @commands.cooldown(1, 120, commands.BucketType.guild)
+    async def goldendoggos(self, ctx, amount : int = 5):
+        """Throws a golden retriever bomb!
+        Defaults to 5, max is 10"""
+        results = []
+        if amount > 10 or amount < 1:
+            amount = 5
+        try:
+            for x in range(0,amount):
+                async with self.session.get(self.goldenapi) as r:
+                    api_result = await r.json()
+                    results.append(api_result['message'])
+            await ctx.send("\n".join(results))
+        except:
+            await ctx.send("API Error")
 
     def __unload(self):
         self.bot.loop.create_task(self.session.close())
-    __del__ = __unload
+
+__del__ = __unload
