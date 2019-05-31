31a3774939a6157b53992b6d3010a7f5297ae4d1
Darius0069 <39631501+Darius0069@users.noreply.github.com>
2019-05-31 09:27:11 -0500

    Update gretriever.py

--- gretriever/gretriever.py
+++ gretriever/gretriever.py
@@ -11,7 +11,6 @@ from redbot.core import commands
 import aiohttp
 
 goldenapi = "https://dog.ceo/api/breed/retriever/golden/images/random"
-huskyapi = "https://dog.ceo/api/breed/husky/images/random"
 
 BaseCog = getattr(commands, "Cog", object)
 
@@ -51,34 +50,6 @@ class Gretriever(BaseCog):
             await ctx.send("\n".join(results))
         except:
             await ctx.send("API Error")
-
-        @commands.command()
-        @commands.cooldown(1, 10, commands.BucketType.guild)
-        async def huskydoggo(self, ctx):
-            """Shows a random husky photo"""
-            try:
-                async with self.session.get(self.huskyapi) as r:
-                    result = await r.json()
-                await ctx.send(result['message'])
-            except:
-                await ctx.send("API Error")
-
-        @commands.command()
-        @commands.cooldown(1, 10, commands.BucketType.guild)
-        async def huskydoggos(self, ctx, amount : int = 5):
-            """Throws a husky bomb!
-            Defaults to 5, max is 10 (thanks Eslyium#1949 & Yukirin#0048)"""
-            results = []
-            if amount > 10 or amount < 1:
-                amount = 5
-            try:
-                for x in range(0,amount):
-                    async with self.session.get(self.huskyapi) as r:
-                        api_result = await r.json()
-                        results.append(api_result['message'])
-                await ctx.send("\n".join(results))
-            except:
-                await ctx.send("API Error")
 
     def __unload(self):
         self.bot.loop.create_task(self.session.close())
