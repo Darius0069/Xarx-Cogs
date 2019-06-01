0afae985e15bb45c439680699e9edc3e1f7da63e
Darius0069 <39631501+Darius0069@users.noreply.github.com>
2019-06-01 18:40:06 +0300

    Update hs.py

--- hs/hs.py
+++ hs/hs.py
@@ -1,28 +1,35 @@
-import discord
-from redbot.core import commands
-import urllib.request
+from discord.ext import commands
 import aiohttp
-import requests
+import re
 
-BaseCog = getattr(commands, "Cog", object)
 
-class HS(BaseCog):
-    """WIP HS Thing"""
-
+class HS:
+    """Le YouTube Cog"""
     def __init__(self, bot):
         self.bot = bot
+        self.youtube_regex = (
+          r'(https?://)?(www\.)?'
+          '(youtube|youtu|youtube-nocookie)\.(com|be)/'
+          '(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})')
 
-url = 'https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/ysera'
+    @commands.command(pass_context=True, name='hs', no_pm=True)
+    async def _youtube(self, context, *, query: str):
+        """Search on Youtube"""
+        try:
+            url = 'https://www.youtube.com/results?'
+            payload = {'search_query': ''.join(query)}
+            headers = {'user-agent': 'Red-cog/1.0'}
+            conn = aiohttp.TCPConnector()
+            session = aiohttp.ClientSession(connector=conn)
+            async with session.get(url, params=payload, headers=headers) as r:
+                result = await r.text()
+            session.close()
+            yt_find = re.findall(r'href=\"\/watch\?v=(.{11})', result)
+            url = 'https://www.youtube.com/watch?v={}'.format(yt_find[0])
+            await self.bot.say(url)
+        except Exception as e:
+            message = 'Something went terribly wrong! [{}]'.format(e)
+            await self.bot.say(message)
 
-param = {
-    "X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com",
-    "X-RapidAPI-Key": "wXIuOpjmlRmsheQch0AYHKBPlGb0p1Z2Zf5jsnyZ5RwvU48gKY"
-}
-
-response = requests.get(url, headers=param)
-
-@commands.command()
-async def hsping(self, ctx):
-    """This does stuff!"""
-    # Your code will go here
-    await ctx.send(response)
+    def __unload(self):
+        self.bot.loop.create_task(self.session.close())
