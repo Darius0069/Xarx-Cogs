e5b3991ba7446a01b01d46d67dee88258ce37504
Darius0069 <39631501+Darius0069@users.noreply.github.com>
2019-06-01 15:54:38 +0300

    Update hs.py

--- hs/hs.py
+++ hs/hs.py
@@ -1,8 +1,8 @@
 import discord
-import aiohttp
 from redbot.core import commands
 import urllib.request
-from bs4 import BeautifulSoup
+import unirest
+import aiohttp
 
 BaseCog = getattr(commands, "Cog", object)
 
@@ -12,23 +12,15 @@ class HS(BaseCog):
     def __init__(self, bot):
         self.bot = bot
 
+response = unirest.get("https://irythia-hs.p.rapidapi.com/card?name=Ysera",
+  headers={
+    "X-RapidAPI-Host": "irythia-hs.p.rapidapi.com",
+    "X-RapidAPI-Key": "wXIuOpjmlRmsheQch0AYHKBPlGb0p1Z2Zf5jsnyZ5RwvU48gKY"
+  }
+)
+
     @commands.command()
     async def hsping(self, ctx):
         """This does stuff!"""
         # Your code will go here
-        await ctx.send("I can do stuff!")
-
-    @commands.command()
-    async def card(self, *text):
-        """Retrieves card art from a hearthpwn search"""
-        if len(text) > 0:
-            try:
-                card = '+'.join([str(x) for x in text]) # convert the text list into a string joined by + characters for use in a web address
-                url = "http://www.hearthpwn.com/cards?filter-name="+card #build the web adress
-                soupObject = BeautifulSoup(urllib.request.urlopen(url), "html.parser") # get the web page and create a BeautifulSoup4 object
-                img = soupObject.find("img")["src"] #find the first image tag and return the source attribute
-                await self.bot.say(img)
-            except:
-                await self.bot.say("`Could not find that card, check your spelling or try another card.`")
-        else:
-            await self.bot.say("```card [name]\n\nSearches http://www.hearthpwn.com/\nReturns first available card that matches the search text.\nUse \"cardg\" to get gold cards.```")
+        await ctx.send(response)
