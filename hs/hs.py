d9c08659a2839b7029146eccbb8ec0326c4a6969
Darius0069 <39631501+Darius0069@users.noreply.github.com>
2019-06-01 18:52:22 +0300

    Update hs.py

--- hs/hs.py
+++ hs/hs.py
@@ -1,35 +1,31 @@
 from discord.ext import commands
 import aiohttp
-import re
-
+try: # check if BeautifulSoup4 is installed
+	from bs4 import BeautifulSoup
+	soupAvailable = True
+except:
+	soupAvailable = False
 
 class HS:
-    """Le YouTube Cog"""
+    """idk if I'll ever get this fucking shit to work"""
     def __init__(self, bot):
         self.bot = bot
-        self.youtube_regex = (
-          r'(https?://)?(www\.)?'
-          '(youtube|youtu|youtube-nocookie)\.(com|be)/'
-          '(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})')
+	if soupAvailable:
+        self.bot = bot
+	else:
+		raise RuntimeError("You need to run `pip3 install beautifulsoup4`")
 
-    @commands.command(pass_context=True, name='hs', no_pm=True)
-    async def _youtube(self, ctx, *, query: str):
-        """Search on Youtube"""
+    @commands.cooldown(1, 10, commands.BucketType.guild)
+    async def hscard(self, ctx):
+        """Should someday make it to search for a fucking card"""
+        url = "https://steamdb.info/app/570/graphs/" #build the web adress
+        async with aiohttp.get(url) as response:
+            soupObject = BeautifulSoup(await response.text(), "html.parser")
         try:
-            url = 'https://www.youtube.com/results?'
-            payload = {'search_query': ''.join(query)}
-            headers = {'user-agent': 'Red-cog/1.0'}
-            conn = aiohttp.TCPConnector()
-            session = aiohttp.ClientSession(connector=conn)
-            async with session.get(url, params=payload, headers=headers) as r:
-                result = await r.text()
-            session.close()
-            yt_find = re.findall(r'href=\"\/watch\?v=(.{11})', result)
-            url = 'https://www.youtube.com/watch?v={}'.format(yt_find[0])
-            await self.bot.say(url)
-        except Exception as e:
-            message = 'Something went terribly wrong! [{}]'.format(e)
-            await self.bot.say(message)
+            online = soupObject.find(class_='home-stats').find('li').find('strong').get_text()
+            await self.bot.say(online + ' players are playing this game at the moment')
+        except:
+            await self.bot.say("Couldn't load amount of players. No one is playing this game anymore or there's an error.")
 
-    def __unload(self):
+        def __unload(self):
         self.bot.loop.create_task(self.session.close())
