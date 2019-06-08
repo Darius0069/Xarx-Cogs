9e941b7bacb000bf2f59c6021bd24719d08ffe90
Darius0069 <39631501+Darius0069@users.noreply.github.com>
2019-06-08 21:39:49 +0300

    Update xarxhs.py

--- xarxhs/xarxhs.py
+++ xarxhs/xarxhs.py
@@ -22,49 +22,27 @@ class Xarxhs(BaseCog):
         self.session = aiohttp.ClientSession(loop=self.bot.loop)
         self.zam = zam
 
-#    @commands.command()
-#    @commands.bot_has_permissions(embed_links=True)
-#    async def xarxjson(self, ctx, name_or_id):
-#        """Show pokemon info"""
-#
-#        try:
-#            headers = {"content-type": "application/json", "X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com", "X-RapidAPI-Key": "wXIuOpjmlRmsheQch0AYHKBPlGb0p1Z2Zf5jsnyZ5RwvU48gKY"}
-#
-#            # Queries pokeapi for Name, ID and evolution_chain
-#            async with aiohttp.ClientSession() as session:
-#                async with session.get("https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/" + name_or_id.lower(), headers=headers) as r1:
-#                    response1 = await r1.json()
-#
-#        except:
-#            await ctx.send("No card found bro")
-#            return
-#
-#        if response1 is None:
-#            await ctx.send("No card found bro 2")
-#        else:
-#            await ctx.send(box(response1, lang='java'))
+    @commands.command()
+    @commands.bot_has_permissions(embed_links=True)
+    async def acard(self, ctx, name_or_id):
+        """Show pokemon info"""
 
-        @commands.command()
-        @commands.bot_has_permissions(embed_links=True)
-        async def acard(self, ctx, acardname):
-            """Show pokemon info"""
+        try:
+            headers = {"content-type": "application/json", "X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com", "X-RapidAPI-Key": "wXIuOpjmlRmsheQch0AYHKBPlGb0p1Z2Zf5jsnyZ5RwvU48gKY"}
 
-            try:
-                headers = {"content-type": "application/json", "X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com", "X-RapidAPI-Key": "wXIuOpjmlRmsheQch0AYHKBPlGb0p1Z2Zf5jsnyZ5RwvU48gKY"}
+            # Queries pokeapi for Name, ID and evolution_chain
+            async with aiohttp.ClientSession() as session:
+                async with session.get("https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/" + name_or_id.lower(), headers=headers) as r1:
+                    response1 = await r1.json()
 
-                # Queries shit
-                async with aiohttp.ClientSession() as session:
-                    async with session.get("https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/" + name_or_id.lower(), headers=headers) as r1:
-                        response1 = await r1.json()
+        except:
+            await ctx.send("No card found bro")
+            return
 
-            except:
-                await ctx.send("No card found bro")
-                return
-
-            if response1 is None:
-                await ctx.send("No card found bro 2")
-            else:
-                await ctx.send(box(response1, lang='java'))
+        if response1 is None:
+            await ctx.send("No card found bro 2")
+        else:
+            await ctx.send(box(response1, lang='java'))
 
 #        # Build Embed
 #        embed = discord.Embed()
