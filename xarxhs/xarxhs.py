5e2f6b8474926676b0dd073a8fed37e3ab2be935
Darius0069 <39631501+Darius0069@users.noreply.github.com>
2019-06-08 21:04:38 +0300

    Update xarxhs.py

--- xarxhs/xarxhs.py
+++ xarxhs/xarxhs.py
@@ -24,7 +24,7 @@ class Xarxhs(BaseCog):
 
     @commands.command()
     @commands.bot_has_permissions(embed_links=True)
-    async def cardg(self, ctx, name_or_id):
+    async def xarxjson(self, ctx, name_or_id):
         """Show pokemon info"""
 
         try:
@@ -42,7 +42,29 @@ class Xarxhs(BaseCog):
         if response1 is None:
             await ctx.send("No card found bro 2")
         else:
-            await ctx.send(box(response1, lang='json'))
+            await ctx.send(box(response1, lang='java'))
+
+        @commands.command()
+        @commands.bot_has_permissions(embed_links=True)
+        async def xarxcard(self, ctx, name_or_id):
+            """Show pokemon info"""
+
+            try:
+                headers = {"content-type": "application/json", "X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com", "X-RapidAPI-Key": "wXIuOpjmlRmsheQch0AYHKBPlGb0p1Z2Zf5jsnyZ5RwvU48gKY"}
+
+                # Queries pokeapi for Name, ID and evolution_chain
+                async with aiohttp.ClientSession() as session:
+                    async with session.get("https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/" + name_or_id.lower(), headers=headers) as r1:
+                        response1 = await r1.json()
+
+            except:
+                await ctx.send("No card found bro")
+                return
+
+            if response1 is None:
+                await ctx.send("No card found bro 2")
+            else:
+                await ctx.send(box(response1['cardId'], lang='ini'))
 
 #        # Build Embed
 #        embed = discord.Embed()
