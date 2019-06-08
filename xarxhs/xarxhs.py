3857f8a55db4b1cd59a76da4b9d94eaa62cd0722
Darius0069 <39631501+Darius0069@users.noreply.github.com>
2019-06-08 20:54:55 +0300

    Update xarxhs.py

--- xarxhs/xarxhs.py
+++ xarxhs/xarxhs.py
@@ -41,7 +41,7 @@ class Xarxhs(BaseCog):
         if response1 is None:
             await ctx.send("No card found bro 2")
         else:
-            print(response1['']['0']['name'])
+            print(response1['name'])
 
 #        # Build Embed
 #        embed = discord.Embed()
