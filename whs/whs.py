68509fb5faf635030b31ffba9c289d80ed5271bb
Darius0069 <39631501+Darius0069@users.noreply.github.com>
2019-06-08 22:19:54 +0300

    WHS

---
+++ whs/whs.py
@@ -0,0 +1,26 @@
+# Post animal pics by Eslyium#1949 & Yukirin#0048 is the original thingy
+# I'm just using it for golden retrievers because of Lyutenitsa
+
+# Discord
+import discord
+# Red
+from redbot.core import commands
+# Libs
+import requests
+import aiohttp
+
+BaseCog = getattr(commands, "Cog", object)
+
+class Xarxhs(BaseCog):
+    """Someday it might be just like a real cog"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.session = aiohttp.ClientSession(loop=self.bot.loop)
+
+    @commands.command()
+    @commands.bot_has_permissions(manage_webhooks=True)
+    async def hshook(self, ctx, lfcard):
+        """Keep calm sigh"""
+        webhook = Webhook.from_url('https://hearthstoneapi.com/webhook/slack', adapter=AsyncWebhookAdapter(session))
+        await Webhook.send('I'm working!, username=lfcard)
