66704c8e7861bf6e135cdcba7216af8892d079a6
Darius0069 <39631501+Darius0069@users.noreply.github.com>
2019-06-08 19:12:24 +0300

    Update xarxhs.py

--- xarxhs/xarxhs.py
+++ xarxhs/xarxhs.py
@@ -33,7 +33,7 @@ class Xarxhs(BaseCog):
             async with aiohttp.ClientSession() as session:
                 async with session.get("https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/" + name_or_id.lower(), headers=headers) as r1:
                     response1 = await r1.json()
-                await ctx.send(response1['img'])
+                await ctx.send(response1)
 
         except:
             await ctx.send("No card found")
