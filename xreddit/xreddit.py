7c1c16fa7ebcfdcd17cedf105886025df65e2dad
Darius0069 <39631501+Darius0069@users.noreply.github.com>
2019-06-16 19:48:56 +0300

    Update xreddit.py

--- xreddit/xreddit.py
+++ xreddit/xreddit.py
@@ -13,8 +13,8 @@ class Xreddit(Core, commands.Cog):
     """
     @commands.bot_has_permissions(embed_links=True)
     @commands.cooldown(1, 0.5, commands.BucketType.user)
-    @commands.command(name="FOODPORN", aliases=["Foodporn", "FoodPorn"])
+    @commands.command(name="foodporn", aliases=["Foodporn", "FoodPorn", "FOODPORN"])
     async def foodporn(self, ctx):
         """Show some foodporn images from random subreddits."""
 
-        await self._send_msg(ctx, constants("Foodporn"), sub=sub.FOODPORN)
+        await self._send_msg(ctx, "Foodporn", sub=sub.FOODPORN)
