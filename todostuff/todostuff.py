c4153c73d39cd73db75e4617d912737646a84af5
Darius0069 <39631501+Darius0069@users.noreply.github.com>
2019-06-19 12:51:21 -0500

    Update todostuff.py

--- todostuff/todostuff.py
+++ todostuff/todostuff.py
@@ -11,15 +11,18 @@ class Todostuff(BaseCog):
 
     def __init__(self):
         self.config = Config.get_conf(self, identifier=900697142, force_registration=True)
-        default_user = {"Stuffs": ""}
-        self.config.register_global(**default_global)
-        self.config.register_global(
-            stuffs=("Nothing")
+        self.config.register_user(
+            todolist={}
         )
 
 @commands.command()
-async def todo(self, ctx, addtodo):
-    user_group = self.config.user(ctx.user)
-    async with user_group.todo() as todo1:
-        todo.append(addtodo)
-    await ctx.send("The new [todo] value has been added!")
+async def testtodo(self, ctx):
+    currentlist = self.config.user(ctx.author).todolist
+    await ctx.send(currentlist)
+
+#@commands.command()
+#async def todo(self, ctx, addtodo):
+#    user_group = self.config.user(ctx.user)
+#    async with user_group.todo() as todo1:
+#        todo.append(addtodo)
+#    await ctx.send("The new [todo] value has been added!")
