495084591876203e924cc759664dfb3619fa23c3
Darius0069 <39631501+Darius0069@users.noreply.github.com>
2019-06-19 10:02:38 -0500

    Update todostuff.py

--- todostuff/todostuff.py
+++ todostuff/todostuff.py
@@ -1,2 +1,23 @@
 import asyncio
 import discord
+import random
+from redbot.core import Config
+from redbot.core import commands
+
+class Todostuff(BaseCog):
+    """I ain't got an idea if this will ever work"""
+
+    def __init__(self):
+        self.config = Config.get_conf(self, identifier=900697142) force_registration=True
+        default_user = {"Stuffs": ""}
+        self.config.register_global(**default_global)
+        self.config.register_global(
+            stuffs=("Nothing")
+        )
+
+@commands.command()
+async def todo(self, ctx, addtodo):
+    user_group = self.config.user(ctx.user)
+    async with user_group.todo() as todo1:
+        todo.append(addtodo)
+    await ctx.send("The new [todo] value has been added!")
